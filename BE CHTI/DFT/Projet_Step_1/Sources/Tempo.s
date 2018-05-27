5f6eb5479d24afa5cb5024f93ed4edafcb4487a3
Baudelair <manuf83@hotmail.fr>
2018-05-27 16:58:27 +0200

    CHTI

--- BE CHTI/DFT/Projet_Step_1/Sources/Tempo.s
+++ BE CHTI/DFT/Projet_Step_1/Sources/Tempo.s
@@ -43,8 +43,6 @@ reelle proc
 	
 	
 debut	
-	
-	
 
 	ldrsh r5,[r0,r4, lsl #1];chargement signal 
 	mul r6,r4,r7; multiplie i par k 
@@ -67,61 +65,4 @@ debut
 	endp
 	end
 	
-	
-	
-		
-	
-	
-	
-
-	
-	
-	
-	
-	
-	
-	; if signal 1 = 0, passe a 1 et envoie a GPIO_BSRR, sinon 0 et envoie
-	
-	;ldr r1, =signal1 	; r1 adresse de signal1
-	;ldr r2, [r1]		; r5 valeur de signal1
-	
-	
-		;CBZ r2 , misea1
-		
-			;push {r1,r2}
-			;; mise a zero de PB1
-			;ldr	r3, =GPIOB_BSRR
-			;movs	r1, #0x00020000
-			;str	r1, [r3]
-			
-			;pop {r1,r2}
-			;mov	r2 , #0		;signal a 0
-			;str r2, [r1]
-			;b lafin
-;misea1
-			;push {r1,r2}
-			;; mise a 1 de PB1
-			;ldr	r3, =GPIOB_BSRR
-			;movs	r1, #0x00000002
-			;str	r1, [r3]	
-
-			;pop {r1,r2}
-			;mov	r2 , #1		;signal a 1
-			;str r2, [r1]
-
-;lafin
-	;bx lr
-	;endp
-	;end
-;; mise a 1 de PB1
-;;	ldr	r3, =GPIOB_BSRR
-;;	movs	r1, #0x00000002
-;;	str	r1, [r3]
-;; mise a zero de PB1
-;;	ldr	r3, =GPIOB_BSRR
-;;	movs	r1, #0x00020000
-;;	str	r1, [r3]
-;;	end 			
-				
-
 	
